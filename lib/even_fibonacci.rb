2a5fab08658e2955c9646cc6c753c559ef5947f4
makafis <vinnyalfieri@gmail.com>
2015-07-02 09:23:00 -0400

    complete

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,17 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(numb)
+array = [1,2]
+prev_num = 0
+current_num = 1
+
+
+while array[current_num] < numb
+  array << array[prev_num] + array[current_num]
+  prev_num +=1
+  current_num +=1
+end
+if array[-1] > numb
+  array.pop
+end
+array.select(&:even?).reduce(:+)
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,25 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+  def initialize(numb)
+    @numb = numb
+  end
+
+  def sum
+    array = [1,2]
+    prev_num = 0
+    current_num = 1
+  
+
+while array[current_num] < @numb
+  array << array[prev_num] + array[current_num]
+  prev_num +=1
+  current_num +=1
+end
+if array[-1] > @numb
+  array.pop
+end
+array.select(&:even?).reduce(:+)
+  end
+
+
+end
